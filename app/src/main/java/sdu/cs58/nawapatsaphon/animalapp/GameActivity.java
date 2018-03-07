4f5af891a465f8030b751a901483cd3e8da5a954
nawapatsaphon <nawapatsaphon@gmail.com>
2018-03-07 10:37:03 +0700

    UI Design Complete

---
+++ app/src/main/java/sdu/cs58/nawapatsaphon/animalapp/GameActivity.java
@@ -0,0 +1,16 @@
+package sdu.cs58.nawapatsaphon.animalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }//end onCreate method
+
+}//end Class
+
